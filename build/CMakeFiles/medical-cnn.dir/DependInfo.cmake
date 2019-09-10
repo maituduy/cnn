605ac3b0607e3cc862d26c50f8a9785ff8763659
maituduy <maituduy1998@gmail.com>
2019-09-10 12:44:37 +0700

    1009

---
+++ build/CMakeFiles/medical-cnn.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xmw/dev/medical-cnn/src/activation.cpp" "/home/xmw/dev/medical-cnn/build/CMakeFiles/medical-cnn.dir/src/activation.cpp.o"
+  "/home/xmw/dev/medical-cnn/src/main.cpp" "/home/xmw/dev/medical-cnn/build/CMakeFiles/medical-cnn.dir/src/main.cpp.o"
+  "/home/xmw/dev/medical-cnn/src/nn_ops.cpp" "/home/xmw/dev/medical-cnn/build/CMakeFiles/medical-cnn.dir/src/nn_ops.cpp.o"
+  "/home/xmw/dev/medical-cnn/src/ops_util.cpp" "/home/xmw/dev/medical-cnn/build/CMakeFiles/medical-cnn.dir/src/ops_util.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/medical-cnn.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/medical-cnn.dir/src/activation.cpp.o"
+  "CMakeFiles/medical-cnn.dir/src/main.cpp.o"
+  "CMakeFiles/medical-cnn.dir/src/nn_ops.cpp.o"
+  "CMakeFiles/medical-cnn.dir/src/ops_util.cpp.o"
+  "medical-cnn.pdb"
+  "medical-cnn"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/medical-cnn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
